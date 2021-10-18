251647037a55058c0f414f3a8e51fb4dffc89d7e
9Flights <john.net6005@gmail.com>
2021-10-18 13:32:28 +0530

    in feature 3 we can add pax source and destination

---
+++ WebApplication1/Pax1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Pax1
+    {
+        public string Source { get; set; }
+        public string Destination { get; set; }
+    }
+}
