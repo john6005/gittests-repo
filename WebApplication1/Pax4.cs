2d3016b8de1b7773a05a1c9a79991249bd6c6327
9Flights <john.net6005@gmail.com>
2021-10-18 14:39:35 +0530

    added ititnary deatils in feature5

---
+++ WebApplication1/Pax4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Pax4
+    {
+
+
+        public string itinaryno { get; set; }
+    }
+}
