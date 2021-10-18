9281e9a0a93e6e6b7411fc65b9e55890cf856345
9Flights <john.net6005@gmail.com>
2021-10-18 15:29:57 +0530

    adding Index method

--- WebApplication1/Pax1.cs
+++ WebApplication1/Pax1.cs
@@ -9,5 +9,15 @@ namespace WebApplication1
     {
         public string Source { get; set; }
         public string Destination { get; set; }
+        public string Index()
+        {
+            Pax1 p1 = new Pax1()
+            {
+            
+           
+            };
+
+            return "";
+        }
     }
 }
