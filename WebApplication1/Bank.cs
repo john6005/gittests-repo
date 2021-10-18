3fe1d253e3ec120dd3c408478f5d129324caa40f
9Flights <john.net6005@gmail.com>
2021-10-18 13:16:02 +0530

    in feature1 we can added two details

---
+++ WebApplication1/Bank.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Bank
+    {
+        public string PaxId { get; set; }
+        public string PaxName { get; set; }
+    }
+}
